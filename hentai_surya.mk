9be56431f7929c3aacf612cb8c2cb66fccb14204
Jiyong Park <jiyong@google.com>
2019-04-15 14:19:03 +0900

    surya: Add updatable_apex.mk
    The product abstracts configs that need to be set to support updating of APEXes.
    
    Signed-off-by: Unicote <arvogl874@gmail.com>

--- hentai_surya.mk
+++ hentai_surya.mk
@@ -17,6 +17,7 @@
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
 
 # Inherit from surya device
 $(call inherit-product, device/xiaomi/surya/device.mk)
