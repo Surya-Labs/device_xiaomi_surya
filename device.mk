82b0b610894b4d61643ad9f5dedf6bf90720556f
XsaviXander <22182013+XsaviXander@users.noreply.github.com>
2021-03-01 14:28:29 +0800

    surya: Enable freeform windows
    Signed-off-by: Sonal Singh <sonal.singh.19993@gmail.com>
    Signed-off-by: David Setiawan <fryevia@foxmail.com>
    Signed-off-by: chrisw444 <wandersonrodriguesf1@gmail.com>

--- device.mk
+++ device.mk
@@ -148,6 +148,10 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.software.freeform_window_management.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.freeform_window_management.xml
 
+# Freeform Multiwindow
+PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/android.software.freeform_window_management.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.freeform_window_management.xml
+
 # GPS
 PRODUCT_PACKAGES += \
     android.hardware.gnss@2.1-impl-qti \
