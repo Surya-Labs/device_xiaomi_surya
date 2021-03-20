54fbc15c0b9c3d41fbc630e853287c2a80408b22
Adithya R <gh0strider.2k18.reborn@gmail.com>
2021-03-20 17:35:50 +0530

    surya: Define DT2W node for screen-off gestures
    Signed-off-by: Unicote <arvogl874@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -73,6 +73,9 @@ TARGET_USES_HWC2 := true
 # DRM
 TARGET_ENABLE_MEDIADRM_64 := true
 
+# DT2W
+TARGET_TAP_TO_WAKE_NODE  := "/proc/tp_gesture"
+
 # Filesystem
 TARGET_FS_CONFIG_GEN := $(DEVICE_PATH)/configs/config.fs
 
