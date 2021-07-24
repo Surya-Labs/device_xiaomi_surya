1afa44309a272a2c4c6c8647926363d22711d208
chrisw444 <wandersonrodriguesf1@gmail.com>
2021-07-24 18:20:43 -0300

    surya: GO dotOS
    Signed-off-by: chrisw444 <wandersonrodriguesf1@gmail.com>
    Signed-off-by: chrisw444 <chrisw444@pixelexperience.org>

--- dot_surya.mk
+++ dot_surya.mk
@@ -18,14 +18,14 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk)
 
-# Inherit from common AOSP configuration
-$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
+# Inherit from common DotOS configuration
+$(call inherit-product, vendor/dot/config/common.mk)
 
 # Inherit from surya device
 $(call inherit-product, device/xiaomi/surya/device.mk)
 
 # Device identifier
-PRODUCT_NAME := aosp_surya
+PRODUCT_NAME := dot_surya
 PRODUCT_DEVICE := surya
 PRODUCT_BRAND := POCO
 PRODUCT_MODEL := POCO X3
