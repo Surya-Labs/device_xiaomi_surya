18d467e9ac67d39cc3dce27bc70f7ce7c5c9099b
Subhajeet Muhuri <subhajeet.muhuri@gmail.com>
2021-09-28 19:50:02 +0530

    surya: rro_overlays: Set default Settings.Global.DEVICE_NAME for karna & surya
     * Reference:
       https://github.com/AdarshGrewal/android_device_xiaomi_sweet/commit/a4524e8d6648ec8e7f98e7d779d11eb98a0f5103
    
    Signed-off-by: chrisw444 <chrisw444@pixelexperience.org>

--- device.mk
+++ device.mk
@@ -303,11 +303,15 @@ PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += \
 
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
-    $(LOCAL_PATH)/overlay-dot
+    $(LOCAL_PATH)/overlay-aosp
 
 PRODUCT_PACKAGES += \
-    KarnaFrameworks 
-    
+    KarnaFrameworks \
+    KarnaSettingsProviderOverlay \
+    SuryaSettingsProviderOverlay \
+    NoCutoutOverlay \
+    NotchBarKiller
+
 # Partitions
 PRODUCT_BUILD_SUPER_PARTITION := false
 PRODUCT_USE_DYNAMIC_PARTITIONS := true
