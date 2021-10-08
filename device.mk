12d72cc5c3c6e04a27a9f8b342be3f22f19ee68f
Gustavo Mendes <gusttavo.me@outlook.com>
2021-10-08 08:24:25 -0300

    surya: overlay: Adapt to PE
    * Increase statusbar size
    * Nuke NoCutoutOverlay
    * Nuke NotchBarKiller

--- device.mk
+++ device.mk
@@ -306,10 +306,8 @@ DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay-dot
 
 PRODUCT_PACKAGES += \
-    KarnaFrameworks \
-    NoCutoutOverlay \
-    NotchBarKiller
-
+    KarnaFrameworks 
+    
 # Partitions
 PRODUCT_BUILD_SUPER_PARTITION := false
 PRODUCT_USE_DYNAMIC_PARTITIONS := true
